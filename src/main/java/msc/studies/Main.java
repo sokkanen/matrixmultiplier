15b1679a4146259369462982bad3d299220b50c2
sokkanen <joel.sokkanen@helsinki.fi>
2023-01-28 21:53:15 +0200

    Added memory usage information.

--- src/main/java/msc/studies/Main.java
+++ src/main/java/msc/studies/Main.java
@@ -6,11 +6,24 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        printMemoryUsage();
         try {
             Multiplier sm = new Multiplier();
             SimpleMatrix res = sm.run();
         } catch (IOException exception) {
             System.err.println("ERROR" + exception.getMessage());
         }
+        printMemoryUsage();
+    }
+
+    public static void printMemoryUsage() {
+        int MB = 1048576;
+
+        Runtime rt = Runtime.getRuntime();
+        System.out.println("\nJVM Heap stats in MB");
+        System.out.printf("Total: %d\n", (rt.totalMemory() / MB));
+        System.out.printf("Free: %d\n", (rt.freeMemory() / MB));
+        System.out.printf("Used: %d\n", (rt.totalMemory() - rt.freeMemory()) / MB);
+        System.out.printf("Max: %d\n\n", (rt.maxMemory() / MB));
     }
 }
